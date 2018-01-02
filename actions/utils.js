5d6e3b7109b7a1283f32864f44276872107a2e4e
Rohit Nandan <rohitn@dewsolutions.in>
2018-01-02 17:13:22 +0530

    Purge

--- actions/utils.js
+++
@@ -1,9 +0,0 @@
-import { TOGGLE_NAV } from '../types/utils';
-
-
-export function toggleNav({toggle}) {
-  return {
-    type: TOGGLE_NAV,
-    toggle
-  };
-}
--- services/products.js
+++
@@ -1,9 +0,0 @@
-import { urls, post, get } from '../plugins/api'
-
-export const getProducts = function(sessionId, skip, limit){
-  return (get(urls.videos(sessionId, skip, limit)))
-}
-
-export const getProduct = function(sessionId, videoId){
-  return (get(urls.video(sessionId, videoId)))
-}
